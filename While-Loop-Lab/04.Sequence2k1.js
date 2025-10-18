e4d4807024c50d637b20a0223e48eaaf7ca07a22
unknown <ganchin94@gmail.com>
2025-10-18 21:56:18 +0300

    new task

---
+++ While-Loop-Lab/04.Sequence2k1.js
@@ -0,0 +1,19 @@
+function sequence(number){
+    
+    let start = 1;
+    let step = start * 2 + 1;
+    let sum = 0;
+    
+
+    while (start <= number){
+      
+      console.log(start);
+      start = start * 2 + 1;
+      
+      
+    }
+    
+  }
+    
+
+sequence(31)
