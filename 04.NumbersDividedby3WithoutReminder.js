a1dcff1475481726d10038dc909ceb1f3e4a9161
unknown <ganchin94@gmail.com>
2025-10-22 22:46:15 +0300

    new task

---
+++ 04.NumbersDividedby3WithoutReminder.js
@@ -0,0 +1,11 @@
+function numbers(){
+
+  for (let i = 1; i < 100;i++){
+  while(i % 3 === 0){
+    console.log(i);
+    break;
+    
+  }
+}
+}
+numbers()
---
+++ 05.AverageNumber.js
@@ -0,0 +1,19 @@
+function numbersAvg(input){
+  let wagers = Number(input.shift()); 
+  let index = 0;
+  let sum = 0;
+
+  while (index < input.length){
+    sum += Number(input[index]);
+    index++;
+  }
+  if (wagers === 0){
+  }else {
+  console.log((sum / wagers).toFixed(2));
+  }
+}
+numbersAvg(["1",
+"95",
+"23",
+"76",
+"23"])
