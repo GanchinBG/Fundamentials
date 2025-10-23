157f8ff96fe4b4da4299b6af4c47ba6c564ec06a
unknown <ganchin94@gmail.com>
2025-10-23 19:40:48 +0300

    try with For Loop for last 10 %

--- While-Loop-MoreExercises/05.AverageNumber.js
+++ While-Loop-MoreExercises/05.AverageNumber.js
@@ -12,12 +12,19 @@ function numbersAvg(input){
   console.log((sum / wagers).toFixed(2));
   }
 }
-numbersAvg(["1",
-"95",
-"23",
-"76",
-"23"])
+numbersAvg
 
 function average(input){
-  
+  let sum = 0;
+  let wagers = Number(input.shift());
+    for (let i = 0; i < input.length;i++){
+      sum += Number(input[i]);
+      
+    }
+    console.log((sum / wagers).toFixed(2));
 }
+average(["4",
+"3",
+"2",
+"4",
+"2"])
